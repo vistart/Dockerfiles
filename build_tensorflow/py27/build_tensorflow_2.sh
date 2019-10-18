d48a4bd9df3d39fcf297024ef04d0c4fcabc56a3
Zhao vistart <i@vistart.me>
2019-10-18 23:13:15 +0800

    [skip ci]add missing quotes.

--- build_tensorflow/py27/build_tensorflow_2.sh
+++ build_tensorflow/py27/build_tensorflow_2.sh
@@ -1,4 +1,4 @@
-docker run -itd --runtime=nvidia --rm --name build_tensorflow_2_py27_cuda10.0 -w /root/tensorflow -v $PWD:/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:1.15-py27-cuda10.0
+docker run -itd --runtime=nvidia --rm --name build_tensorflow_2_py27_cuda10.0 -w /root/tensorflow -v "$PWD":/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:1.15-py27-cuda10.0
 docker exec -it build_tensorflow_2_py27_cuda10.0 git checkout r2.0
 docker exec -it build_tensorflow_2_py27_cuda10.0 cp /mnt/build_tensorflow/.tf_configure.bazelrc.py27 .tf_configure.bazelrc
 docker exec -it build_tensorflow_2_py27_cuda10.0 bazel build //tensorflow/tools/pip_package:build_pip_package
--- build_tensorflow/py36-cuda10.1/build_tensorflow_2.sh
+++ build_tensorflow/py36-cuda10.1/build_tensorflow_2.sh
@@ -1,4 +1,4 @@
-docker run -itd --runtime=nvidia --rm --name build_tensorflow_2_py36_cuda10.1 -w /root/tensorflow -v $PWD:/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:1.15-py36-cuda10.1
+docker run -itd --runtime=nvidia --rm --name build_tensorflow_2_py36_cuda10.1 -w /root/tensorflow -v "$PWD":/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:1.15-py36-cuda10.1
 docker exec -it build_tensorflow_2_py36_cuda10.1 git checkout r2.0
 docker exec -it build_tensorflow_2_py36_cuda10.1 cp /mnt/build_tensorflow/.tf_configure.bazelrc.py36 .tf_configure.bazelrc
 docker exec -it build_tensorflow_2_py36_cuda10.1 bazel build //tensorflow/tools/pip_package:build_pip_package

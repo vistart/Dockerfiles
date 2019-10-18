73f20edd3de712d9904c6e485855916ef10f7854
Zhao vistart <i@vistart.me>
2019-10-18 22:18:00 +0800

    [skip ci]fix typo.

--- build_tensorflow/py27-cuda10.1/build_tensorflow_1.sh
+++ build_tensorflow/py27-cuda10.1/build_tensorflow_1.sh
@@ -1,4 +1,4 @@
 docker run -itd --runtime=nvidia --rm --name build_tensorflow_1_py27_cuda10.1 -w /root/tensorflow -v $PWD:/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:1.15-py27-cuda10.1
-docker exec -it build_tensorflow_1_py27_cuda10.0 git checkout r1.15
+docker exec -it build_tensorflow_1_py27_cuda10.1 git checkout r1.15
 docker exec -it build_tensorflow_1_py27_cuda10.1 bazel build //tensorflow/tools/pip_package:build_pip_package
 docker exec -it build_tensorflow_1_py27_cuda10.1 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt

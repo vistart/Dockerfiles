f25b3b173c1c58affbce8dfcbbdaa429727b0df9
Zhao vistart <i@vistart.me>
2019-10-18 21:36:34 +0800

    [skip ci]add scripts for building tensorflow.

---
+++ build_tensorflow/py27/build_tensorflow_1.sh
@@ -0,0 +1,3 @@
+docker run -itd --runtime=nvidia --rm --name build_tensorflow_1_py27_cuda10.0 -w /root/tensorflow -v $PWD:/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:1.15-py27-cuda10.0
+docker exec -it build_tensorflow_1_py27_cuda10.0 bazel build //tensorflow/tools/pip_package:build_pip_package
+docker exec -it build_tensorflow_1_py27_cuda10.0 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt
---
+++ build_tensorflow/py27/build_tensorflow_2.sh
@@ -0,0 +1,4 @@
+docker run -itd --runtime=nvidia --rm --name build_tensorflow_2_py27_cuda10.0 -w /root/tensorflow -v $PWD:/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:1.15-py27-cuda10.0
+docker exec -it build_tensorflow_2_py27_cuda10.0 git checkout r2.0
+docker exec -it build_tensorflow_2_py27_cuda10.0 bazel build //tensorflow/tools/pip_package:build_pip_package
+docker exec -it build_tensorflow_2_py27_cuda10.0 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt

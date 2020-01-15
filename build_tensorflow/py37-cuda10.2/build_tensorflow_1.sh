bec0a86ce06957d8dc2bee8d34be7fce3074d13b
vistart <i@vistart.me>
2020-01-15 19:22:05 +0800

    fix missing packages.

--- build_tensorflow/py37-cuda10.2/build_tensorflow_1.sh
+++ build_tensorflow/py37-cuda10.2/build_tensorflow_1.sh
@@ -1,4 +1,4 @@
-docker run -itd --runtime=nvidia --rm --name build_tensorflow_1_py36_cuda10.2-tf1 -w /root/tensorflow -v "$PWD":/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:py36-cuda10.2-tf1
-docker exec -it build_tensorflow_1_py36_cuda10.2-tf1 bazel build //tensorflow/tools/pip_package:build_pip_package
-docker exec -it build_tensorflow_1_py36_cuda10.2-tf1 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt
-docker stop build_tensorflow_1_py36_cuda10.2-tf1
+docker run -itd --runtime=nvidia --rm --name build_tensorflow_1_py37_cuda10.2-tf1 -w /root/tensorflow -v "$PWD":/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:py37-cuda10.2-tf1
+docker exec -it build_tensorflow_1_py37_cuda10.2-tf1 bazel build //tensorflow/tools/pip_package:build_pip_package
+docker exec -it build_tensorflow_1_py37_cuda10.2-tf1 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt
+docker stop build_tensorflow_1_py37_cuda10.2-tf1
--- build_tensorflow/py37-cuda10.2/build_tensorflow_2.sh
+++ build_tensorflow/py37-cuda10.2/build_tensorflow_2.sh
@@ -1,4 +1,4 @@
-docker run -itd --runtime=nvidia --rm --name build_tensorflow_2_py36_cuda10.2-tf2 -w /root/tensorflow -v "$PWD":/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:py36-cuda10.2-tf2
-docker exec -it build_tensorflow_2_py36_cuda10.2-tf2 bazel build //tensorflow/tools/pip_package:build_pip_package
-docker exec -it build_tensorflow_2_py36_cuda10.2-tf2 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt
-docker stop build_tensorflow_2_py36_cuda10.2-tf2
+docker run -itd --runtime=nvidia --rm --name build_tensorflow_2_py37_cuda10.2-tf2 -w /root/tensorflow -v "$PWD":/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:py37-cuda10.2-tf2
+docker exec -it build_tensorflow_2_py37_cuda10.2-tf2 bazel build //tensorflow/tools/pip_package:build_pip_package
+docker exec -it build_tensorflow_2_py37_cuda10.2-tf2 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt
+docker stop build_tensorflow_2_py37_cuda10.2-tf2

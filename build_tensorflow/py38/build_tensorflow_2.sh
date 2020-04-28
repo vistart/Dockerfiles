08163e18ed8a46fb459318816b9ff5eed89f664c
Zhao vistart <i@vistart.me>
2020-04-28 23:08:13 +0800

    update to Python 3.8 in Ubuntu 20.04.

---
+++ build_tensorflow/py38/build_tensorflow_2.sh
@@ -0,0 +1,4 @@
+docker run -itd --runtime=nvidia --rm --name build_tensorflow_2_py38_cuda10.0-tf2 -w /root/tensorflow -v "$PWD":/mnt -e HOST_PERMS="$(id -u):$(id -g)" vistart/build_tensorflow:py38-cuda10.0-tf2
+docker exec -it build_tensorflow_2_py38_cuda10.0-tf2 bazel build //tensorflow/tools/pip_package:build_pip_package
+docker exec -it build_tensorflow_2_py38_cuda10.0-tf2 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt
+docker stop build_tensorflow_2_py38_cuda10.0-tf2
